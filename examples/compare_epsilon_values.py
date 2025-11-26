f98d190d1d0986511489e9ebf8537e30e026e724
ChiuJung <loustrand28@gmail.com>
2025-11-26 12:42:42 +0800

    forth commit

--- examples/compare_epsilon_values.py
+++ examples/compare_epsilon_values.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     env = GridWorld()
     
     for eps in epsilon_values:
-        policy, values = train_agent(eps, num_episodes=5000, max_steps=30)
+        policy, values = train_agent(eps, num_episodes=5000, max_steps=100)
         print_state_values(eps, values, env.env_size)
         visualize_policy(eps, policy, values)
     
